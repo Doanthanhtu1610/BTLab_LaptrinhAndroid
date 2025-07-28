d18ee46ce77b03ac827a20a9cdccb8222b66b212
Doanthanhtu1610 <2251172540@e.tlu.edu.vn>
2025-07-29 00:23:58 +0700

    cap nhat bai2

---
+++ Bai22/app/src/main/java/com/example/bai2/Subactivity.java
@@ -0,0 +1,28 @@
+package com.example.bai2;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class subactivity extends AppCompatActivity {
+    Button btnok;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+}
