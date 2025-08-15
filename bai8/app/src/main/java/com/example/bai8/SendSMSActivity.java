1e44a19bfdee8d85fdfda47b25b7608bf7d9ab4b
Doanthanhtu1610 <2251172540@e.tlu.edu.vn>
2025-08-15 11:37:33 +0700

    cap nhat bai 8

--- bai8/app/src/main/java/com/example/bai8/SendSMSActivity.java
+++ bai8/app/src/main/java/com/example/bai8/SendSMSActivity.java
@@ -1,9 +1,12 @@
 package com.example.bai8;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,35 +14,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SendSMSActivity extends AppCompatActivity {
 
-    Button btnCallPhone, btnSendSMS;
+    EditText edtSMS;
+    ImageButton btnCall;
+    Button btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_send_smsactivity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnCallPhone = findViewById(R.id.btnCallPhone);
-        btnCallPhone.setOnClickListener(new View.OnClickListener() {
+        edtSMS = findViewById(R.id.edtcall);
+        btnCall = findViewById(R.id.btnsms);
+        btnBack = findViewById(R.id.btnBack);
+        btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(MainActivity.this, CallPhoneActivity.class);
-                startActivity(intent1);
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+                smsIntent.setData(Uri.parse("smsto:"));
+                smsIntent.putExtra("sms_body", edtSMS.getText().toString());
+                startActivity(smsIntent);
             }
         });
 
-        btnSendSMS = findViewById(R.id.btnSendSms);
-        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(MainActivity.this, SendSMSActivity.class);
-                startActivity(intent2);
+                finish();
             }
         });
     }
